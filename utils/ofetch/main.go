4783a3ffe164d2550470ba45258279c20e079c73
Jamie Alquiza <jamiealquiza@gmail.com>
2016-04-29 11:42:30 -0600

    properly format empty output

--- utils/ofetch/main.go
+++ utils/ofetch/main.go
@@ -23,78 +23,83 @@
 package main
 
 import (
-        "bufio"
-        "flag"
-        "fmt"
-        "net"
-        "sync"
-        "time"
+	"bufio"
+	"flag"
+	"fmt"
+	"net"
+	"sync"
+	"time"
 
-        "github.com/jamiealquiza/cidrxpndr"
+	"github.com/jamiealquiza/cidrxpndr"
 )
 
 var Settings struct {
-        c    int
-        t    int
-        net  string
-        port string
+	c    int
+	t    int
+	net  string
+	port string
 }
 
 func init() {
-        flag.IntVar(&Settings.c, "c", 256, "request concurrency")
-        flag.IntVar(&Settings.t, "t", 25, "request timeout in ms")
-        flag.StringVar(&Settings.net, "net", "192.168.1.100/32", "network CIDR range")
-        flag.StringVar(&Settings.port, "port", "8080", "ostat listening port")
-        flag.Parse()
+	flag.IntVar(&Settings.c, "c", 256, "request concurrency")
+	flag.IntVar(&Settings.t, "t", 25, "request timeout in ms")
+	flag.StringVar(&Settings.net, "net", "192.168.1.100/32", "network CIDR range")
+	flag.StringVar(&Settings.port, "port", "8080", "ostat listening port")
+	flag.Parse()
 }
 
 func main() {
-        ips, _ := cidrxpndr.Expand(Settings.net)
+	ips, _ := cidrxpndr.Expand(Settings.net)
 
-        var wg sync.WaitGroup
-        wg.Add(Settings.c)
+	var wg sync.WaitGroup
+	wg.Add(Settings.c)
 
-        nodes := make(chan string, Settings.c+256)
-        response := make(chan []byte, len(ips))
+	nodes := make(chan string, Settings.c+256)
+	response := make(chan []byte, len(ips))
 
-        for i := 0; i < Settings.c; i++ {
-                go requester(nodes, response, &wg)
-        }
+	for i := 0; i < Settings.c; i++ {
+		go requester(nodes, response, &wg)
+	}
 
-        for _, i := range ips {
-                nodes <- i
-        }
+	for _, i := range ips {
+		nodes <- i
+	}
 
-        close(nodes)
-        wg.Wait()
-        close(response)
+	close(nodes)
+	wg.Wait()
+	close(response)
 
-        output := []byte{91}
-        for i := range response {
-                output = append(output, i[:len(i)-1]...)
-                output = append(output, 44)
-        }
-        output[len(output)-1] = 93
+	output := []byte{91}
+	for i := range response {
+		output = append(output, i[:len(i)-1]...)
+		output = append(output, 44)
+	}
 
-        fmt.Println(string(output))
+	if len(output) > 1 {
+		output[len(output)-1] = 93
+	} else {
+		output = []byte{91, 93}
+	}
+
+	fmt.Println(string(output))
 }
 
 func requester(n chan string, r chan []byte, wg *sync.WaitGroup) {
-        for h := range n {
-                c, err := net.DialTimeout("tcp",
-                        h+":"+Settings.port,
-                        time.Duration(time.Millisecond*time.Duration(Settings.t)))
-                if err != nil {
-                        continue
-                }
+	for h := range n {
+		c, err := net.DialTimeout("tcp",
+			h+":"+Settings.port,
+			time.Duration(time.Millisecond*time.Duration(Settings.t)))
+		if err != nil {
+			continue
+		}
 
-                fmt.Fprintf(c, "stats")
-                resp, err := bufio.NewReader(c).ReadBytes(10)
-                if err != nil {
-                        continue
-                } else {
-                        r <- resp
-                }
-        }
-        wg.Done()
+		fmt.Fprintf(c, "stats")
+		resp, err := bufio.NewReader(c).ReadBytes(10)
+		if err != nil {
+			continue
+		} else {
+			r <- resp
+		}
+	}
+	wg.Done()
 }
